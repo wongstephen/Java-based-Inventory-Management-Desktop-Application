e3f1a8ef26b1959dc8441ed0ebfd3f9e14ea84d0
Stephen Wong <wongstephenky@gmail.com>
2023-04-12 17:49:27 -0700

    refractored navigation to be its own function

--- src/main/java/ims/inventorymanagementsystem/controller/MainController.java
+++ src/main/java/ims/inventorymanagementsystem/controller/MainController.java
@@ -17,32 +17,34 @@ public class MainController implements Initializable {
     Stage stage;
     Parent scene;
 
-    public void handleMainSearchPartSubmit(ActionEvent actionEvent) {
+    @FXML
+    void handleMainSearchPartSubmit(ActionEvent actionEvent) {
         System.out.println("Enter Search Part Pressed");
     }
 
     // Opens Add Parts Menu in new scene
     // RUNTIME ERROR when there is no IOException. GetClass().getResource did not work. Used code from new project template Main.class.getResource to get it functional.
     // FUTURE ENHANCEMENT refactor in to try catch block.
-    @FXML
-    void handleMainAddPartBtn(ActionEvent actionEvent) throws IOException {
+
+    void navigate(ActionEvent actionEvent, String location) throws IOException{
         stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(Main.class.getResource("view/addPart.fxml"));
+        scene = FXMLLoader.load(Main.class.getResource("view/"+location+".fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
     }
+
+    @FXML
+    void handleMainAddPartBtn(ActionEvent actionEvent) throws IOException {
+        navigate(actionEvent, "addPart");
+    }
     @FXML
     void handleMainModifyPartBtn(ActionEvent actionEvent) throws IOException {
-        stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(Main.class.getResource("view/modifyPart.fxml"));
-        stage.setScene(new Scene(scene));
-        stage.show();
+        navigate(actionEvent, "modifyPart");
     }
     @FXML
     void handleMainDeletePartBtn(ActionEvent actionEvent) {
         System.out.println("Main Delete Clicked");
     }
-
 
     @FXML
     void handleMainSearchProductSubmit(ActionEvent actionEvent) {
@@ -50,11 +52,14 @@ public class MainController implements Initializable {
     }
 
     //    Opens Add Products Menu in new scene
-    public void handleMainAddProductBtn(ActionEvent actionEvent) {
-        System.out.println("Main Add Product Pressed");
-
+    @FXML
+    void handleMainAddProductBtn(ActionEvent actionEvent) throws IOException {
+        navigate(actionEvent, "addProduct");
     }
-
+    @FXML
+    void handleMainModifyProductBtn(ActionEvent actionEvent) throws IOException {
+        navigate(actionEvent, "modifyProduct");
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
