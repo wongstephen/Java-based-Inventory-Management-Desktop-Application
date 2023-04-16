cd00b135327a98cac0914f1f32e1a1380ea369ee
Stephen Wong <wongstephenky@gmail.com>
2023-04-15 22:23:19 -0700

    added fields from part fxml to partcontroller

--- src/main/java/ims/stephenwongc482/controller/AddPartController.java
+++ src/main/java/ims/stephenwongc482/controller/AddPartController.java
@@ -3,15 +3,27 @@ package ims.stephenwongc482.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import static ims.stephenwongc482.controller.NavController.navigate;
 
 
 public class AddPartController {
+    public TextField nameInput;
+    public TextField stockInput;
+    public TextField priceInput;
+    public TextField minInput;
+    public TextField maxInput;
+    public TextField sourceInput;
+    public RadioButton inHouseRadio;
+    public RadioButton outsourcedRadio;
 
 
+    Scanner sc = new Scanner(System.in);
     public Label sourceLabel;
 
     /**
@@ -22,7 +34,16 @@ public class AddPartController {
     @FXML
     void handleSaveBtn(ActionEvent actionEvent) throws IOException {
         // Save Data then navigate to main
-        navigate(actionEvent, "mainScreen");
+
+        System.out.println(inHouseRadio.isSelected());
+        System.out.println(outsourcedRadio.getText());
+//        System.out.println(nameInput.getText());
+//        System.out.println(stockInput.getText());
+//        System.out.println(priceInput.getText());
+//        System.out.println(minInput.getText());
+//        System.out.println(maxInput.getText());
+//        System.out.println(maxInput.getText());
+//        navigate(actionEvent, "mainScreen");
     }
 
     /**
@@ -52,4 +73,6 @@ public class AddPartController {
     public void onOutsourced(ActionEvent actionEvent) {
         sourceLabel.setText("Company Name");
     }
+
+
 }
